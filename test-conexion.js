ef6d6f461692f4f8aa80947293b6206b675821b6
infopronto-maker <info.pronto@icloud.com>
2026-07-06 10:10:14 -0500

    Update test-conexion.js

--- test-conexion.js
+++ test-conexion.js
@@ -1,14 +1,18 @@
 const API_KEY = process.env.API_FOOTBALL_KEY;
 
-async function probarConexion() {
-  const response = await fetch('https://v3.football.api-sports.io/status', {
+async function traerPartidosDeHoy() {
+  const hoy = new Date().toISOString().split('T')[0];
+
+  const response = await fetch(`https://v3.football.api-sports.io/fixtures?date=${hoy}`, {
     headers: {
       'x-apisports-key': API_KEY
     }
   });
 
   const data = await response.json();
-  console.log(JSON.stringify(data, null, 2));
+  console.log("Partidos encontrados:", data.results);
+  console.log(JSON.stringify(data.response.slice(0, 3), null, 2));
 }
 
-probarConexion();
+traerPartidosDeHoy();
+
